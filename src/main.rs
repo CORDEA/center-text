4f366c1d8fa3d9aa0cb9b7172acf2fd93c1b113b
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-15 20:01:33 +0900

    draw rect

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,22 @@ fn main() {
     draw_text_mut(&mut image, Rgba([0, 0, 0, 255]), 0, 0, scale, &font, text);
 
     let red = Rgba([255, 0, 0, 255]);
-    for glyph in glyphs {
+    for glyph in glyphs.iter() {
         let rect = imageproc::rect::Rect::at(glyph.min.x, glyph.min.y)
             .of_size(glyph.width() as u32, glyph.height() as u32);
         draw_hollow_rect_mut(&mut image, rect, red);
     }
+
+    let blue = Rgba([0, 255, 0, 255]);
+
+    let first = glyphs.first().unwrap().min;
+    let last = glyphs.last().unwrap().max;
+    let min_y = glyphs.iter().map(|g| g.min.y).min().unwrap();
+    let max_y = glyphs.iter().map(|g| g.max.y).max().unwrap();
+
+    let rect = imageproc::rect::Rect::at(first.x, min_y)
+        .of_size((last.x - first.x) as u32, (max_y - min_y) as u32);
+    draw_hollow_rect_mut(&mut image, rect, blue);
 
     let _ = image.save("hello.png").unwrap();
 }
