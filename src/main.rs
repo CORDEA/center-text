f8cd00535f4ea86c5adb88edb32ae686b0ce82bf
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-15 20:30:11 +0900

    center text

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate imageproc;
 extern crate rusttype;
 
 use imageproc::drawing::{draw_text_mut, draw_hollow_rect_mut};
-use image::Rgba;
+use image::{GenericImageView, Rgba};
 use rusttype::{FontCollection, Scale, Rect, point};
 
 fn main() {
@@ -16,11 +16,21 @@ fn main() {
     let scale = Scale { x: 100.0, y: 100.0 };
     let point = point(0.0, font.v_metrics(scale).ascent);
 
+    let (w, h) = image.dimensions();
     let glyphs: Vec<Rect<i32>> = font.layout(text, scale, point)
         .map(|g| g.pixel_bounding_box().unwrap())
         .collect();
 
-    draw_text_mut(&mut image, Rgba([0, 0, 0, 255]), 0, 0, scale, &font, text);
+    let first = glyphs.first().unwrap().min;
+    let last = glyphs.last().unwrap().max;
+    let min_y = glyphs.iter().map(|g| g.min.y).min().unwrap();
+    let max_y = glyphs.iter().map(|g| g.max.y).max().unwrap();
+    let height = max_y - min_y;
+    let width = last.x - first.x;
+    let center_x = (w / 2) - (width / 2) as u32 - first.x as u32;
+    let center_y = (h / 2) - (height / 2) as u32 - min_y as u32;
+
+    draw_text_mut(&mut image, Rgba([0, 0, 0, 255]), center_x, center_y, scale, &font, text);
 
     let red = Rgba([255, 0, 0, 255]);
     for glyph in glyphs.iter() {
