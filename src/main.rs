3a2e5bc1a103fd783a8cdb954b569688fc16d540
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-15 19:18:59 +0900

    draw text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate image;
+extern crate imageproc;
+extern crate rusttype;
+
+use imageproc::drawing::draw_text_mut;
+use image::Rgba;
+use rusttype::{FontCollection, Scale};
+
 fn main() {
-    println!("Hello, world!");
+    let mut image = image::open("white.png").unwrap();
+
+    let font = Vec::from(include_bytes!("Roboto-Black.ttf") as &[u8]);
+    let font = FontCollection::from_bytes(font).unwrap().into_font().unwrap();
+
+    let scale = Scale { x: 100.0, y: 100.0 };
+    draw_text_mut(&mut image, Rgba([0u8, 0u8, 0u8, 1u8]), 0, 0, scale, &font, "hello");
+
+    let _ = image.save("hello.png").unwrap();
 }
